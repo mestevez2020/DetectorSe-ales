29c2c62a8ce3cbb01c4292022609a6f5b4d1835d
albertoam3 <albertoarcones3@gmail.com>
2024-04-24 19:43:31 +0200

    detector añadido

--- Detector.py
+++ Detector.py
@@ -15,8 +15,6 @@ def convert_to_grayscale(image_path):
     cv2.destroyAllWindows()
 
 
-
-
 def enhance_contrast(image_path):
     # Cargar la imagen en color
     original_image = cv2.imread(image_path)
@@ -49,16 +47,22 @@ def enhance_contrast(image_path):
     plt.title('Histograma de la imagen con contraste mejorado')
     plt.show()
 
+    return equ_image
 
 def apply_mser(image_path):
     # Cargar la imagen en color
     original_image = cv2.imread(image_path)
 
-    # Convertir la imagen a escala de grises
-    gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+    # Verificar si la imagen fue cargada correctamente
+    if original_image is None:
+        print(f"No se pudo cargar la imagen desde {image_path}")
+        return
+
+    # Convertir la imagen a escala de grises y filtrala
+    gray_image = enhance_contrast(image_path)
 
     # Crear el detector MSER con parámetros personalizados
-    mser = cv2.MSER_create(_delta=5, _min_area=100, _max_area=2000)
+    mser = cv2.MSER_create(delta=5, min_area=100, max_area=2000)
 
     # Detectar regiones de alto contraste
     regions, _ = mser.detectRegions(gray_image)
@@ -66,18 +70,18 @@ def apply_mser(image_path):
     # Dibujar los contornos de las regiones detectadas sobre la imagen original
     for region in regions:
         x, y, w, h = cv2.boundingRect(region)
-        cv2.rectangle(original_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Calcular la relación de aspecto
+        aspect_ratio = w / float(h)
+
+        # Filtrar regiones por la relación de aspecto
+        if 0.9 <= aspect_ratio <= 1.1:  # Aceptar regiones cuya relación de aspecto es cercana a 1.0
+            cv2.rectangle(original_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     # Mostrar la imagen con las regiones detectadas
     cv2.imshow('MSER', original_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
-
-
-
-
-
 
 
 # El siguiente bloque se usa solo para pruebas directas de este módulo.
@@ -86,6 +90,6 @@ if __name__ == "__main__":
 
     if len(sys.argv) > 1:
         # Aplicar MSER a la imagen con parámetros personalizados
-        convert_to_grayscale(sys.argv[1])
+        apply_mser(sys.argv[1])
     else:
         print("Usage: python detector.py path_to_image")
