b52c6539ba9f07abfb15f0b930671fefcfe94d26
albertoam3 <albertoarcones3@gmail.com>
2024-04-30 10:36:20 +0200

    imagenes filtradas y ordenadas

--- Detector.py
+++ Detector.py
@@ -17,7 +17,7 @@ senal4=[]
 senal5=[]
 senal6=[]
 
-def expand_detected_regions(regions,original_image,datos, expand_factor=1.5):
+def expand_detected_regions(regions, gray_image,original_image, datos, expand_factor=1.5):
     expanded_regions = []
     for region in regions:
         x, y, w, h = cv2.boundingRect(region)
@@ -25,8 +25,8 @@ def expand_detected_regions(regions,original_image,datos, expand_factor=1.5):
         if 0.9 <= aspect_ratio <= 1.1:
             new_x = max(0, int(x - (expand_factor - 1) / 2 * w))
             new_y = max(0, int(y - (expand_factor - 1) / 2 * h))
-            new_w = min(original_image.shape[1], int(w * expand_factor))
-            new_h = min(original_image.shape[0], int(h * expand_factor))
+            new_w = min(gray_image.shape[1], int(w * expand_factor))
+            new_h = min(gray_image.shape[0], int(h * expand_factor))
             encontrado=False
             for dato in datos:
                 if (int(new_x) - 10 <= int(dato[1]) <= int(new_x) + 10 and
@@ -38,24 +38,26 @@ def expand_detected_regions(regions,original_image,datos, expand_factor=1.5):
 
 
                     if encontrado:
+                        imagen_recortada= original_image[new_y:new_y+new_h, new_x:new_x+new_w]
+                        imagen_final_guardada=resize_regions(imagen_recortada)
 
                         if dato[5] in num_senal1:
-                            senal1.append(region)
+                            senal1.append(imagen_final_guardada)
                             expanded_regions.append((new_x, new_y, new_w, new_h))
                         elif dato[5] in num_senal2:
-                            senal2.append(region)
+                            senal2.append(imagen_final_guardada)
                             expanded_regions.append((new_x, new_y, new_w, new_h))
                         elif dato[5] in num_senal3:
-                            senal3.append(region)
+                            senal3.append(imagen_final_guardada)
                             expanded_regions.append((new_x, new_y, new_w, new_h))
                         elif dato[5] in num_senal4:
-                            senal4.append(region)
+                            senal4.append(imagen_final_guardada)
                             expanded_regions.append((new_x, new_y, new_w, new_h))
                         elif dato[5] in num_senal5:
-                            senal5.append(region)
+                            senal5.append(imagen_final_guardada)
                             expanded_regions.append((new_x, new_y, new_w, new_h))
                         elif dato[5] in num_senal6:
-                            senal6.append(region)
+                            senal6.append(imagen_final_guardada)
                             expanded_regions.append((new_x, new_y, new_w, new_h))
 
                     break
@@ -90,17 +92,8 @@ def enhance_contrast(original_image):
 
 
 #Cambiar el tamaño de las imagenes que conseguimos recortando
-def resize_regions(regions, image, target_size=(250, 250)):
-    resized_regions = []
-    for x, y, w, h in regions:
-        region_image = image[y:y+h, x:x+w]
-
-        resized_region = cv2.resize(region_image, target_size)
-
-        resized_regions.append(resized_region)
-
-    return resized_regions
-
+def resize_regions( image, target_size=(250, 250)):
+    return cv2.resize(image, target_size)
 
 def mser_func(original_image, min, max,datos):
     gray_image = enhance_contrast(original_image)
@@ -108,7 +101,7 @@ def mser_func(original_image, min, max,datos):
     mser = cv2.MSER_create(delta=5, min_area=min, max_area=max)
 
     regions, _ = mser.detectRegions(gray_image)
-    expanded_regions = expand_detected_regions(regions, gray_image,datos)
+    expanded_regions = expand_detected_regions(regions, gray_image,original_image, datos)
     return expanded_regions
 
 def apply_mser(image_paths,gt_txt=''):
@@ -129,80 +122,69 @@ def apply_mser(image_paths,gt_txt=''):
         for x, y, w, h in expanded_regions:
 
             cv2.rectangle(original_image, (x, y), (x + w, y + h), (0, 0, 255), 1)
-
-        #cv2.imshow('MSER', original_image)
-        #cv2.waitKey(0)
-
-        resized_regions = resize_regions(expanded_regions, original_image)
-
-
-
+    print("senal1")
     for region in senal1:
-        imagen=np.array(region)
-        cv2.imshow("original", imagen)
+        mascara_roja(region)
+    print("senal2")
+    for region in senal2:
+        mascara_roja(region)
+    print("senal3")
+    for region in senal3:
+        mascara_roja(region)
+    print("senal4")
+    for region in senal4:
+        mascara_roja(region)
+    print("senal5")
+    for region in senal5:
+        mascara_roja(region)
+    print("senal6")
+    for region in senal6:
+        mascara_azul(region)
 
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        detectar_colores(region)
-
-
-#esta parte esta mal
-def detectar_colores(imagen):
-    imagen_gray = np.array(imagen, dtype=np.uint8)
-
-    # Convertir la imagen a formato BGR
-    imagen_bgr = cv2.cvtColor(imagen_gray, cv2.COLOR_GRAY2BGR)
-
-    # Convertir la imagen BGR a HSV
-    imagen_hsv = cv2.cvtColor(imagen_bgr, cv2.COLOR_BGR2HSV)
-
-    rango_azul_bajo = np.array([100, 50, 50])
-    rango_azul_alto = np.array([140, 255, 255])
-
+def mascara_roja(imagen):
+    imagen_hsv = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
     rango_rojo_bajo1 = np.array([0, 50, 50])
     rango_rojo_alto1 = np.array([10, 255, 255])
     rango_rojo_bajo2 = np.array([160, 50, 50])
     rango_rojo_alto2 = np.array([180, 255, 255])
-
-    mascara_azul = cv2.inRange(imagen_hsv, rango_azul_bajo, rango_azul_alto)
-    pixeles_azules = cv2.bitwise_and(imagen_bgr, imagen_bgr, mask=mascara_azul)
-
     mascara_roja1 = cv2.inRange(imagen_hsv, rango_rojo_bajo1, rango_rojo_alto1)
     mascara_roja2 = cv2.inRange(imagen_hsv, rango_rojo_bajo2, rango_rojo_alto2)
     mascara_roja = cv2.bitwise_or(mascara_roja1, mascara_roja2)
-    pixeles_rojos = cv2.bitwise_and(imagen_bgr, imagen_bgr, mask=mascara_roja)
+    pixeles_rojos = cv2.bitwise_and(imagen, imagen, mask=mascara_roja)
 
     pixeles_rojos_gris = cv2.cvtColor(pixeles_rojos, cv2.COLOR_BGR2GRAY)
 
     cantidad_pixeles_rojos = cv2.countNonZero(pixeles_rojos_gris)
 
-    umbral= 6000
+    umbral = 6000
 
-
-    regiones=[]
     print(cantidad_pixeles_rojos)
     if cantidad_pixeles_rojos > umbral:
-        cv2.imshow("original", imagen_bgr)
+        cv2.imshow("original", imagen)
         cv2.imshow('Píxeles Rojos', pixeles_rojos)
 
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-        regiones.append(pixeles_rojos)
 
+def mascara_azul(imagen):
+    imagen_hsv = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
+    rango_azul_bajo = np.array([100, 50, 50])
+    rango_azul_alto = np.array([140, 255, 255])
+
+    mascara_azul = cv2.inRange(imagen_hsv, rango_azul_bajo, rango_azul_alto)
+    pixeles_azules = cv2.bitwise_and(imagen, imagen, mask=mascara_azul)
     pixeles_azules_gris = cv2.cvtColor(pixeles_azules, cv2.COLOR_BGR2GRAY)
 
     cantidad_pixeles_azules = cv2.countNonZero(pixeles_azules_gris)
     umbral = 15000
     if cantidad_pixeles_azules > umbral:
         print(cantidad_pixeles_azules)
-     #   cv2.imshow("original", imagen)
-     #   cv2.imshow('Píxeles Azules', pixeles_azules)
+        cv2.imshow("original", imagen)
+        cv2.imshow('Píxeles Azules', pixeles_azules)
 
-     #   cv2.waitKey(0)
-     #   cv2.destroyAllWindows()
-        regiones.append(pixeles_azules)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-    return regiones
 
 if __name__ == "__main__":
     import sys
